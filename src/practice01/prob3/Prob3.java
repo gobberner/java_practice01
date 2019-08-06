37cbefd357e7d3ac3a5f04862de24e4a61f69968
bit-user <bit-user@DESKTOP-C1QR4BV>
2019-08-06 21:25:31 +0900

    commiy

--- src/practice01/prob3/Prob3.java
+++ src/practice01/prob3/Prob3.java
@@ -1,6 +1,27 @@
 package practice01.prob3;
 
+import java.util.Scanner;
+
 public class Prob3 {
 	public static void main (String args[]) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("수를 입력하세요");
+	
+		int number = scanner.nextInt();
+		int sum = 0;
+		int k;
+		
+		if(number%2 ==0)
+			k=2;
+		else{
+			k=1;
+		}
+		for(int i =k; i<=number; i+=2) {
+			sum+=i;
+		}
+		System.out.println(sum);
 	}
 }
+
--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -2,5 +2,16 @@ package practice01.prob5;
 
 public class Prob5 {
 	public static void main (String args[]) {
+		for(int i =3; i<99; i++) {
+			
+			if( )
+			for(int j= i; j<i+1; j++) {
+				
+				System.out.print(j+" ");
+				
+			}
+			
+			
+		}
 	}
 }
